5f288ec4454f09b55827790e66fe051c90ce632c
Your Name <you@example.com>
2025-07-18 13:37:08 +0300

    Allow access to status endpoint without authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,6 +16,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    '/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api/auth|api/status|login|_next/static|_next/image|favicon.ico).*)',
   ],
 };
